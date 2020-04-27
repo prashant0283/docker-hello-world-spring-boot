a3c29e05c598d8517f6af500b564bd372713714c
mukeshupreti2006 <mukeshupreti2006@gmail.com>
2020-04-28 02:26:08 +0530

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -3,13 +3,17 @@ package com.dockerforjavadevelopers.hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class HelloController {
     
+     @Value("${greeter.message}")
+    private String greeterMessageFormat; 
+    
     @RequestMapping("/")
     public String index() {
-        return "Hello World\n";
+        return greeterMessageFormat;
     }
     
 }
